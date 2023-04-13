3516a507e315fb5328bd46fd785ba702d66800b2
Henthony <henthony555@gmail.com>
2023-04-13 00:49:17 -0300

    aula do C09

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -2,18 +2,32 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 @EqualsAndHashCode(of = { "id" })
 public abstract class EntidadeNegocio implements Serializable {
 
-    private Long id;
+    @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE)
+   private Long id;
 
-    private Boolean habilitado;
+   @JsonIgnore
+   @Column
+   private Boolean habilitado;
+
     
 }
 
